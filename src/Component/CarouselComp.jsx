3463280a5222ac9b7212badcb5841582c60a0a0d
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2025-04-25 16:45:34 +0530

    commit

--- src/Component/CarouselComp.jsx
+++ src/Component/CarouselComp.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { Carousel, Typography, Button } from "@material-tailwind/react";
 function CarouselComp() {
   return (
-    <Carousel className="rounded-xl  h-[400px] w-[970px] overflow-hidden">
+    <Carousel className="rounded-xl  h-full w-full sm:h-[400px] sm:w-[970px] overflow-hidden">
     <div className="relative h-full w-full">
       <img
         src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
         alt="image 3"
         className="h-full w-full object-cover"
       />
-      <div className="absolute bottom-0 inset-0 flex justify-start items-center h-auto w-full items-end bg-black/55">
+      <div className="absolute bottom-0 inset-0 flex justify-start h-auto w-full items-end bg-black/55">
         <div className="absolute w-[80%] left-[10%] bottom-[10%]">
           <Typography
             variant="h1"
             color="white"
-            className="text-3xl md:text-4xl lg:text-5xl"
+            className="text-xl md:text-3xl lg:text-5xl"
           >
             The Beauty of Nature
           </Typography>
           <Typography
             variant="lead"
             color="white"
-            className="mb-5 opacity-80"
+            className="mb-5 text-lg sm:text-xl  opacity-80"
           >
             It is not so much for its beauty that the forest makes a claim
             upon men&apos;s hearts, as for that subtle something, that quality
